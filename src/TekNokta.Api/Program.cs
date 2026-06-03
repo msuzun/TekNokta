5aa694d04cfb8bbc3c6025125fc4feda53fe8e81
msuzun <sevki_uzun53@hotmail.com>
2026-06-03 22:11:11 +0300

    Swagger JWT Authorization Desteği

--- src/TekNokta.Api/Program.cs
+++ src/TekNokta.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi;
 using TekNokta.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +7,27 @@ const string allowAllCorsPolicy = "AllowAll";
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT token'ı şu formatta girin: Bearer {token}"
+    };
+
+    options.AddSecurityDefinition("Bearer", jwtSecurityScheme);
+    options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecuritySchemeReference("Bearer", document, externalResource: null),
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddCors(options =>
 {
